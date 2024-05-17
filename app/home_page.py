00dfadbc04b36f8d7a08d7005fbc8d9e63a3023a
Sivothayan <84649236+Sivothajan@users.noreply.github.com>
2024-05-17 18:01:42 +0530

    Update home_page.py

--- app/home_page.py
+++ app/home_page.py
@@ -1,1 +1,27 @@
+import add_courses
+import calculate_gpa
+import remove_courses
+import data_storage
 
+while True:
+    print("**********************************************************************")
+    print("Welcome to the GPA Calculator!\n")
+
+    print("1.Add Courses\n2.Calculate GPA\n3.Remove Courses\n4.Exit\n")
+
+    choice = input("Enter your Choice: ")
+    print("")
+
+    if(int(choice) == 1):
+        add_courses.new()
+    elif(int(choice) == 2):
+        calculate_gpa.page()
+    elif(int(choice) == 3):
+        remove_courses.remove()
+    elif(int(choice) == 4):
+        data_storage.save_data_json()
+        print("Exited!")
+        break
+    else:
+        print("Invalid Choice!")
+        
